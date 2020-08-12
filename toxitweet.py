86e789db79fde7d0f588fd297fdcf1beb24f3ba7
mphillipsjr96 <66802828+mphillipsjr96@users.noreply.github.com>
2020-08-12 09:57:17 -0500

    add script

---
+++ toxitweet.py
@@ -0,0 +1,45 @@
+from twython import Twython
+import json
+import pandas as pd
+from nltk.corpus import stopwords
+stopwords = list(stopwords.words("english"))
+# Load credentials from json file
+with open("twitter_credentials.json", "r") as file:
+    creds = json.load(file)
+
+badWords = pd.read_csv("C:/Users/micha/Documents/Python/AI/NLP/toxitweet/swearWords.csv").values
+
+# Instantiate an object
+python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+screen_names = ['MichaelPhiIIips', 'FuckMacki', 'fraaaaaaaaank', 'scottyphil', 'MMA_Roundhouse', 'HilvertJack', 'jason_bader', 'ColeSchultz24', 'mpucci10_', 'doonsausy', 'AdamSantiago16', 'davgrif', 'dwrobel328', 'nate_grahovac', 'mikesheps11', 'prodsamyool']
+dict_ = {'handle': [], 'date': [], 'text': []}
+# Create our query
+for user in screen_names:
+    query = {'screen_name': user,
+            "include_rts": False,
+            "count": 100
+            }
+    # Search tweets
+    for status in python_tweets.get_user_timeline(**query):
+        dict_['handle'].append(query['screen_name'])
+        dict_['date'].append(status['created_at'])
+        dict_['text'].append(status['text'])
+
+# Structure data in a pandas DataFrame for easier manipulation
+df = pd.DataFrame(dict_)
+df["text"].replace("(@\S*)|(https:.*)|([^a-zA-z0-9 #*&%$!\.,])","",regex=True, inplace = True)
+df["text"].replace("\A\s+|\s+\Z","",regex=True, inplace = True)
+df = df[df["text"] != ""]
+df["text"] = df["text"].str.split(" ")
+badCounter = []
+for tweet in df["text"]:
+    badCount = 0
+    for i,word in enumerate(tweet):
+        if word.lower() in stopwords:
+            tweet.remove(word)
+        if word.lower() in badWords:
+            badCount += 1
+    badCounter.append(badCount)
+
+df["Toxic"] = badCounter
+df.to_csv("C:/Users/micha/Documents/Python/AI/NLP/toxitweet/tweets.csv")
